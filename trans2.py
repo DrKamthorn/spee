5f87d097ae053e848ff2bc1f91eba36406cb31c2
DrKamthorn <132547814+DrKamthorn@users.noreply.github.com>
2023-05-18 13:49:04 +0700

    Add files via upload

---
+++ trans2.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import whisper
+
+st.set_page_config(layout="wide")
+st.title("งานสารบรรณ ศูนย์การแพทย์กาญจนาภิเษก")
+
+audio_file=st.file_uploader("Upload ไฟล์เสียง", type=["wav","mp3","m4a"])
+
+@st.cache
+def whisper_load_model():
+    return model
+
+model=whisper.load_model("medium")
+st.text("กำลัง Upload ไฟล์")
+
+if st.sidebar.button("Transcribe Audio"):
+	if audio_file is not None:
+		st.sidebar.success("Transcribing Audio")
+		transcription=model.transcribe(audio_file.name)
+		st.sidebar.success("transcription Complete")
+		st.markdown(transcription["text"])
+	else:
+		st.sidebar.error("Please upload an audio file")
+
+st.sidebar.header("Play Original Audio File")
+st.sidebar.audio(audio_file)
